9531f684695dbd0454021f8a730abd429c840af1
Jerome Rasky <jyrome.112@gmail.com>
2015-11-08 15:05:57 -0800

    Add license headers

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,9 @@
+// Copyright 2015 Jerome Rasky <jerome@rasky.co>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT
+// or http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 pub const WHITESPACE_FACTOR: f32 = 5.0;
 pub const WHITESPACE_REDUCE: f32 = 0.5;
 pub const CLASS_FACTOR: f32 = 3.0;
