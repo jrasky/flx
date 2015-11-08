252319a19fb0aab7b8f12052149b10a5ad64a485
Jerome Rasky <jyrome.112@gmail.com>
2015-11-08 14:16:53 -0800

    Improve the library-ness
    Move the search code into a separate file, and make lib.rs the public
    interface definition. Right now that just means pub use the relevant
    structs, but in the future may be more complicated.
    
    TODO: implement some tests

---
+++ src/constants.rs
@@ -0,0 +1,11 @@
+pub const WHITESPACE_FACTOR: isize = 5;
+pub const WHITESPACE_REDUCE: isize = 2;
+pub const CLASS_FACTOR: isize = 3;
+pub const FIRST_FACTOR: isize = 3;
+pub const CLASS_REDUCE: isize = 2;
+
+pub const DIST_WEIGHT: isize = -10;
+pub const HEAT_WEIGHT: isize = 5;
+pub const FACTOR_REDUCE: isize = 50;
+
+pub const MAX_LEN: usize = 80;
